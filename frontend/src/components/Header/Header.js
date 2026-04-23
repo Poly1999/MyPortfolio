71f3d86af165d6d88512c514f8fcbf1c872917e6
Poly1999 <pikalova.developer@gmail.com>
2026-04-23 18:01:42 -0400

    hero section done

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -14,32 +14,30 @@ function Header() {
           </a>
           <p className='header__left-text'>pikalova*</p>
         </div>
-
         <p
           className='header__center'
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          Menu
+          <span className='header__center-label'>Menu</span>
+          {isMenuOpen && (
+            <nav className='header__nav'>
+              <ul className='header__nav-list'>
+                <li className='header__nav-item'>
+                  <a href='#about'>About me</a>
+                </li>
+                <li className='header__nav-item'>
+                  <a href='#projects'>Projects</a>
+                </li>
+                <li className='header__nav-item'>
+                  <a href='#faq'>FAQ</a>
+                </li>
+                <li className='header__nav-item'>
+                  <a href='#contact'>Work together</a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </p>
-        {isMenuOpen && (
-          <nav className='header__nav'>
-            <ul className='header__nav-list'>
-              <li className='header__nav-item'>
-                <a href='#about'>About me</a>
-              </li>
-              <li className='header__nav-item'>
-                <a href='#projects'>Projects</a>
-              </li>
-              <li className='header__nav-item'>
-                <a href='#faq'>FAQ</a>
-              </li>
-              <li className='header__nav-item'>
-                <a href='#contact'>Work together</a>
-              </li>
-            </ul>
-          </nav>
-        )}
-
         <a href='#projects' className='header__button'>
           My projects
         </a>
